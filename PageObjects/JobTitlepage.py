2dad4d5d1a55e33f25e305cc8491b72ec33e2a28
shubham <shubham.vijayvargiya@arcgate.com>
2023-10-20 18:43:34 +0530

    new commit

--- PageObjects/JobTitlepage.py
+++ PageObjects/JobTitlepage.py
@@ -11,6 +11,13 @@ class jobtitle_data:
         self.save_new_job_title_xpath = '//button[@type="submit"]'
         self.total_job_title_xpath = "div.oxd-table-card"
         self.required_job_title_role_xpath = 'form span'
+        self.job_description_validation_xpath = "//div[@class='oxd-layout-context']//div[2]//div[1]//span[1]"
+        self.job_note_validation_xpath = "//div[4]//div[1]//span[1]"
+        self.cancel_button_click_xpath = 'button.oxd-button--ghost'
+        self.button_delete_job_title_xpath = '.bi-trash'
+        self.button_confirmation_box_yes_xpath = 'button.orangehrm-button-margin:nth-child(2)'
+        self.job_title_exceed_value = '3333333333333333333333333333333333333333333333333333333333333333333333333333333333353333333333333332d'
+        self.job_description_exceed_value = 'ascascascascascaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasasascascascascascaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasasascascascascascaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasasascascascascascaaaaggggggggggggc'
         
     def click_add_new_job_title(self):
         self.driver.find_element(By.CSS_SELECTOR, self.button_add_job_title_xpath).click()
@@ -37,4 +44,25 @@ class jobtitle_data:
         return self.driver.find_element(By.CSS_SELECTOR, self.required_job_title_role_xpath).text
     
     def exceed_limit_of_new_job_title(self):
-        self.driver.find_element(By.CSS_SELECTOR, self.add_new_job_title_xpath).send_keys('3333333333333333333333333333333333333333333333333333333333333333333333333333333333353333333333333332d')
+        self.driver.find_element(By.CSS_SELECTOR, self.add_new_job_title_xpath).send_keys(self.job_title_exceed_value)
+        
+    def exceed_limit_for_job_description(self):
+        self.driver.find_element(By.XPATH,self.add_job_description_xpath).send_keys(self.job_description_exceed_value)
+        
+    def exceed_limit_for_note(self):
+        self.driver.find_element(By.XPATH, self.add_new_add_note_xpath).send_keys(self.job_description_exceed_value)
+
+    def job_description_required_validation(self):
+        return self.driver.find_element(By.XPATH,self.job_description_validation_xpath).text
+    
+    def job_note_required_validation(self):
+        return self.driver.find_element(By.XPATH,self.job_note_validation_xpath).text
+    
+    def click_on_cancel_button(self):
+        self.driver.find_element(By.CSS_SELECTOR,self.cancel_button_click_xpath).click()
+        
+    def click_delete_job_title_button(self):
+        self.driver.find_element(By.CSS_SELECTOR,self.button_delete_job_title_xpath).click()
+        
+    def click_confirmation_box_button_yes(self):
+        self.driver.find_element(By.CSS_SELECTOR,self.button_confirmation_box_yes_xpath).click()
--- TestCase/test_add_job_title.py
+++ TestCase/test_add_job_title.py
@@ -16,30 +16,31 @@ config.read("Utilities/input.properties")
 class TestNewJobTitle(logclass):
     def test_new_job_title_with_correct_input_data(self):
         log = self.getthelogs()
-        lg = Login(self.driver)
-        db = dashboard_data(self.driver)
-        es = jobtitle_data(self.driver)
+        login = Login(self.driver)
+        dashboard = dashboard_data(self.driver)
+        employeement_status = jobtitle_data(self.driver)
         log.info('TEST CASE, test_new_job_title_with_correct_input_data')
         log.info('Test case starting')
-        lg.input_username(config.get("credential", "correct_username"))
-        lg.input_password(config.get("credential", "correct_password"))
-        lg.click_login()
-        db.click_admin()
+        login.input_username(config.get("credential", "correct_username"))
+        login.input_password(config.get("credential", "correct_password"))
+        login.click_login()
+        dashboard.click_admin()
         time.sleep(2)
-        db.click_job()
-        db.click_job_title_section()
+        dashboard.click_job()
+        dashboard.click_job_title_section()
         old_job_title = 0
-        for i in es.total_job_title_data():
+        for i in employeement_status.total_job_title_data():
             old_job_title = old_job_title + 1   
-        es.click_add_new_job_title()
-        es.input_new_job_title_role(status_generator())
-        es.input_job_description(status_generator())
-        es.input_image_upload()
-        es.input_new_add_note(status_generator())
+        employeement_status.click_add_new_job_title()
+        employeement_status.input_new_job_title_role(status_generator())
+        employeement_status.input_job_description(status_generator())
+        employeement_status.input_image_upload()
+        employeement_status.input_new_add_note(status_generator())
         time.sleep(3)
-        es.save_all_data_of_job_title()
+        employeement_status.save_all_data_of_job_title()
+        time.sleep(4)
         new_job_title = 0
-        for j in es.total_job_title_data():
+        for j in employeement_status.total_job_title_data():
             new_job_title = new_job_title + 1
         if old_job_title + 1 == new_job_title:
             assert True
@@ -50,26 +51,26 @@ class TestNewJobTitle(logclass):
         
     def test_new_job_title_with_fill_only_job_title_role_another_field_blank_input(self):
         log = self.getthelogs()
-        lg = Login(self.driver)
-        db = dashboard_data(self.driver)
-        es = jobtitle_data(self.driver)
+        login = Login(self.driver)
+        dashboard = dashboard_data(self.driver)
+        employeement_status = jobtitle_data(self.driver)
         log.info('TEST CASE, test_new_job_title_with_fill_only_job_title_role_another_field_blank_input')
         log.info('Test case starting')
-        lg.input_username(config.get("credential", "correct_username"))
-        lg.input_password(config.get("credential", "correct_password"))
-        lg.click_login()
-        db.click_admin()
+        login.input_username(config.get("credential", "correct_username"))
+        login.input_password(config.get("credential", "correct_password"))
+        login.click_login()
+        dashboard.click_admin()
         time.sleep(2)
-        db.click_job()
-        db.click_job_title_section()
+        dashboard.click_job()
+        dashboard.click_job_title_section()
         old_job_title = 0
-        for i in es.total_job_title_data():
+        for i in employeement_status.total_job_title_data():
             old_job_title = old_job_title + 1
-        es.click_add_new_job_title()
-        es.input_new_job_title_role(status_generator())
-        es.save_all_data_of_job_title()
+        employeement_status.click_add_new_job_title()
+        employeement_status.input_new_job_title_role(status_generator())
+        employeement_status.save_all_data_of_job_title()
         new_job_title = 0
-        for j in es.total_job_title_data():
+        for j in employeement_status.total_job_title_data():
             new_job_title = new_job_title + 1
         if old_job_title + 1 == new_job_title:
             assert True
@@ -80,24 +81,24 @@ class TestNewJobTitle(logclass):
 
     def test_new_job_title_with_blank_job_title_role_another_field_input_filled(self):
         log = self.getthelogs()
-        lg = Login(self.driver)
-        db = dashboard_data(self.driver)
-        es = jobtitle_data(self.driver)
+        login = Login(self.driver)
+        dashboard = dashboard_data(self.driver)
+        employeement_status = jobtitle_data(self.driver)
         log.info('TEST CASE, test_new_job_title_with_fill_only_job_title_role_another_field_blank_input')
         log.info('Test case starting')
-        lg.input_username(config.get("credential", "correct_username"))
-        lg.input_password(config.get("credential", "correct_password"))
-        lg.click_login()
-        db.click_admin()
+        login.input_username(config.get("credential", "correct_username"))
+        login.input_password(config.get("credential", "correct_password"))
+        login.click_login()
+        dashboard.click_admin()
         time.sleep(2)
-        db.click_job()
-        db.click_job_title_section()
-        es.click_add_new_job_title()
-        es.input_job_description(status_generator())
-        es.input_image_upload()
-        es.input_new_add_note(status_generator())
-        es.save_all_data_of_job_title()
-        if 'Required' in es.job_title_required_validation():
+        dashboard.click_job()
+        dashboard.click_job_title_section()
+        employeement_status.click_add_new_job_title()
+        employeement_status.input_job_description(status_generator())
+        employeement_status.input_image_upload()
+        employeement_status.input_new_add_note(status_generator())
+        employeement_status.save_all_data_of_job_title()
+        if 'Required' in employeement_status.job_title_required_validation():
             assert True
             log.info('Test Case Pass')
         else:
@@ -106,23 +107,23 @@ class TestNewJobTitle(logclass):
             
     def test_new_job_title_with_all_input_blank_data(self):
         log = self.getthelogs()
-        lg = Login(self.driver)
-        db = dashboard_data(self.driver)
-        es = jobtitle_data(self.driver)
+        login = Login(self.driver)
+        dashboard = dashboard_data(self.driver)
+        employeement_status = jobtitle_data(self.driver)
         log.info('TEST CASE, test_new_job_title_with_all_input_blank_data')
         log.info('Test case starting')
-        lg.input_username(config.get("credential", "correct_username"))
-        lg.input_password(config.get("credential", "correct_password"))
-        lg.click_login()
-        db.click_admin()
+        login.input_username(config.get("credential", "correct_username"))
+        login.input_password(config.get("credential", "correct_password"))
+        login.click_login()
+        dashboard.click_admin()
         time.sleep(2)
-        db.click_job()
-        db.click_job_title_section()
-        es.click_add_new_job_title()
+        dashboard.click_job()
+        dashboard.click_job_title_section()
+        employeement_status.click_add_new_job_title()
         time.sleep(1)
-        es.save_all_data_of_job_title()
+        employeement_status.save_all_data_of_job_title()
         time.sleep(2)
-        if 'Required' in es.job_title_required_validation():
+        if 'Required' in employeement_status.job_title_required_validation():
             assert True
             log.info('Test Case Pass')
         else:
@@ -131,29 +132,118 @@ class TestNewJobTitle(logclass):
     
     def test_new_job_title_with_exceed_limit_new_job_title(self):
         log = self.getthelogs()
-        lg = Login(self.driver)
-        db = dashboard_data(self.driver)
-        es = jobtitle_data(self.driver)
+        login = Login(self.driver)
+        dashboard = dashboard_data(self.driver)
+        employeement_status = jobtitle_data(self.driver)
         log.info('TEST CASE, test_new_job_title_with_all_input_blank_data')
         log.info('Test case starting')
-        lg.input_username(config.get("credential", "correct_username"))
-        lg.input_password(config.get("credential", "correct_password"))
-        lg.click_login()
-        db.click_admin()
+        login.input_username(config.get("credential", "correct_username"))
+        login.input_password(config.get("credential", "correct_password"))
+        login.click_login()
+        dashboard.click_admin()
         time.sleep(2)
-        db.click_job()
-        db.click_job_title_section()
-        es.click_add_new_job_title()
-        es.exceed_limit_of_new_job_title()
-        es.input_job_description(status_generator())
-        es.input_image_upload()
-        es.input_new_add_note(status_generator())
+        dashboard.click_job()
+        dashboard.click_job_title_section()
+        employeement_status.click_add_new_job_title()
+        employeement_status.exceed_limit_of_new_job_title()
+        employeement_status.input_job_description(status_generator())
+        employeement_status.input_image_upload()
+        employeement_status.input_new_add_note(status_generator())
         time.sleep(3)
-        es.save_all_data_of_job_title()
+        employeement_status.save_all_data_of_job_title()
         time.sleep(2)
-        if 'Should not exceed 100 characters' in es.job_title_required_validation():
+        if 'Should not exceed 100 characters' in employeement_status.job_title_required_validation():
             assert True
             log.info('Test Case Pass')
         else:
             log.critical('Test Case fail')
             assert False
+
+    def test_new_job_title_with_exceed_limit_of_all_input_field(self):
+        log = self.getthelogs()
+        login = Login(self.driver)
+        dashboard = dashboard_data(self.driver)
+        employeement_status = jobtitle_data(self.driver)
+        log.info('TEST CASE, test_new_job_title_with_exceed_limit_of_all_input_field')
+        log.info('Test case starting')
+        login.input_username(config.get("credential", "correct_username"))
+        login.input_password(config.get("credential", "correct_password"))
+        login.click_login()
+        dashboard.click_admin()
+        time.sleep(2)
+        dashboard.click_job()
+        dashboard.click_job_title_section()
+        employeement_status.click_add_new_job_title()
+        employeement_status.exceed_limit_of_new_job_title()
+        employeement_status.exceed_limit_for_job_description()
+        employeement_status.exceed_limit_for_note()
+        time.sleep(3)
+        if 'Should not exceed 100 characters' in employeement_status.job_title_required_validation() and 'Should not exceed 400 characters' in employeement_status.job_description_required_validation() and 'Should not exceed 400 characters' in employeement_status.job_note_required_validation():
+            log.info('Test Case Pass')
+            assert True
+        else:
+            log.critical('Test Case Fail')
+            assert False
+            
+    def test_new_job_title_with_cancel_button_click(self):
+        log = self.getthelogs()
+        login = Login(self.driver)
+        dashboard = dashboard_data(self.driver)
+        employeement_status = jobtitle_data(self.driver)
+        log.info('TEST CASE, test_new_job_title_with_cancel_button_click')
+        log.info('Test case starting')
+        login.input_username(config.get("credential", "correct_username"))
+        login.input_password(config.get("credential", "correct_password"))
+        login.click_login()
+        dashboard.click_admin()
+        time.sleep(2)
+        dashboard.click_job()
+        dashboard.click_job_title_section()
+        old_job_title = 0
+        for i in employeement_status.total_job_title_data():
+            old_job_title = old_job_title + 1
+        employeement_status.click_add_new_job_title()
+        time.sleep(3)
+        employeement_status.click_on_cancel_button()
+        time.sleep(3)
+        new_job_title = 0
+        for i in employeement_status.total_job_title_data():
+            new_job_title = new_job_title + 1
+        if old_job_title == new_job_title:
+            assert True
+            log.info('Test Case Pass')
+        else:
+            log.critical('Test Case Fail')
+            assert False
+            
+    def test_delete_job_title(self):
+        log = self.getthelogs()
+        login = Login(self.driver)
+        dashboard = dashboard_data(self.driver)
+        employeement_status = jobtitle_data(self.driver)
+        log.info('TEST CASE, test_delete_job_title')
+        log.info('Test case starting')
+        login.input_username(config.get("credential", "correct_username"))
+        login.input_password(config.get("credential", "correct_password"))
+        login.click_login()
+        dashboard.click_admin()
+        time.sleep(2)
+        dashboard.click_job()
+        dashboard.click_job_title_section()
+        old_job_title = 0
+        for i in employeement_status.total_job_title_data():
+            old_job_title = old_job_title + 1
+        employeement_status.click_delete_job_title_button()
+        time.sleep(2)
+        employeement_status.click_confirmation_box_button_yes()
+        time.sleep(3)
+        new_job_title = 0
+        for j in employeement_status.total_job_title_data():
+            new_job_title = new_job_title + 1
+        if old_job_title - 1 == new_job_title:
+            assert True
+            log.info('Test Case Pass')
+        else:
+            log.critical('Test Case Fail')
+            assert False
+        
